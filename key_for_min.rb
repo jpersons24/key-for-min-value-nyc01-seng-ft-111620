c9bfb764b5d3386173212fb707388274db5104cf
Jakob Persons <jakobep24@gmail.com>
2020-10-22 15:35:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,12 @@
 
 def key_for_min_value(name_hash)
 
+  name_hash.each do |key, value|
+    value = name_hash[key]
+    if value > name_hash[key]
+      low_value = name_hash[key]
+    end
+    low_value
+  end
+
 end
