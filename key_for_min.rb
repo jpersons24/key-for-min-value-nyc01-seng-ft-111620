9ba793d568e6693cf43f9d38cac28f998684e178
Jakob Persons <jakobep24@gmail.com>
2020-10-22 16:18:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,17 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  minimum_k = nil
-  minimum_v = nil
-
   name_hash.each do |k, v|
-    minimum_k = k if minimum_k = nil
-    minimum_v = v if minimum_v = nil
-
-    if minimum_v > v
-      minimum_v = v
-      minimum_k = k
-    end
+    
   end
-  minimum_k
 end
